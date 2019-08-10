44a8396a9d889920d2097b741c782f897df9f8d6
Jessica <rayne1993@gmail.com>
2019-08-10 11:13:33 -0500

    Hit mvp, moving on to stretch

--- assignments/closure.js
+++ assignments/closure.js
@@ -1,6 +1,10 @@
 // ==== Challenge 1: Write your own closure ====
 // Write a simple closure of your own creation.  Keep it simple!
-
+const close = 'blank';
+function rc () {
+  return close;
+}
+console.log(close);
 
 /* STRETCH PROBLEMS, Do not attempt until you have completed all previous tasks for today's project files */
 
